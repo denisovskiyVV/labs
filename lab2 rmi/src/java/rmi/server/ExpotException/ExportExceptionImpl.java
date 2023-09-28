ba07bd28acbdecaaa2ffe3a08ceefd6755ffc17c
denisovskiyVV <126480096+denisovskiyVV@users.noreply.github.com>
2023-09-28 14:24:39 +0300

    Add files via upload

---
+++ lab2 rmi/src/java/rmi/server/ExpotException/ExportExceptionImpl.java
@@ -0,0 +1,9 @@
+package java.rmi.server.ExpotException;
+
+import java.rmi.server.ExportException;
+
+public class ExportExceptionImpl extends ExportException {
+    public ExportExceptionImpl(String s) {
+        super(s);
+    }
+}
